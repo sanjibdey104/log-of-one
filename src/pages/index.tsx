9d746857bc12a18c986161c485ca164e6b6b8e21
sanjib <sanjibdey.dey4@gmail.com>
2025-07-12 17:25:52 +0530

    add: metadata for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { getJournalEntriesList } from "@/lib/fetchJournalEntries";
-import { JournalEntry } from "@/lib/types";
+import Head from "next/head";
 import Link from "next/link";
+import { JournalEntry } from "@/lib/types";
+import { getJournalEntriesList } from "@/lib/fetchJournalEntries";
 
 export default function Home({
   journalDocsList,
@@ -8,43 +9,53 @@ export default function Home({
   journalDocsList: JournalEntry[];
 }) {
   return (
-    <div className="min-h-screen w-full flex flex-col justify-between p-8 gap-32">
-      <header className="w-full p-sm text-center text-xl font-semibold">
-        Welcome to MyLogue
-      </header>
+    <>
+      <Head>
+        <title>MyLogue</title>
+        <meta
+          name="mylogue"
+          content="A space to share my internal reflections and much more..."
+        />
+      </Head>
 
-      <main className="flex-1 w-full max-w-3xl mx-auto">
-        {journalDocsList.length ? (
-          <section className="journal-docs flex flex-col gap-16">
-            <h3>List of journals</h3>
+      <div className="min-h-screen w-full flex flex-col justify-between p-8 gap-32">
+        <header className="w-full p-sm text-center text-xl font-semibold">
+          Welcome to MyLogue
+        </header>
 
-            <ul className="flex flex-col gap-8 list-none p-0">
-              {journalDocsList.map((journalDoc, index) => (
-                <li
-                  key={index}
-                  className="journal-doc flex flex-row justify-between"
-                >
-                  <Link
-                    href={`/journal/${journalDoc.slug}`}
-                    className="journal-name text-blue-900"
+        <main className="flex-1 w-full max-w-3xl mx-auto">
+          {journalDocsList.length ? (
+            <section className="journal-docs flex flex-col gap-16">
+              <h3>List of journals</h3>
+
+              <ul className="flex flex-col gap-8 list-none p-0">
+                {journalDocsList.map((journalDoc, index) => (
+                  <li
+                    key={index}
+                    className="journal-doc flex flex-row justify-between"
                   >
-                    {journalDoc.name}
-                  </Link>
+                    <Link
+                      href={`/journal/${journalDoc.slug}`}
+                      className="journal-name text-blue-900"
+                    >
+                      {journalDoc.name}
+                    </Link>
 
-                  <p className="created-time">{journalDoc.createdTime}</p>
-                </li>
-              ))}
-            </ul>
-          </section>
-        ) : (
-          <p className="no-docs-found">No docs found.</p>
-        )}
-      </main>
+                    <p className="created-time">{journalDoc.createdTime}</p>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          ) : (
+            <p className="no-docs-found">No docs found.</p>
+          )}
+        </main>
 
-      <footer className="w-full text-center text-sm text-gray-400">
-        © 2025 MyLogue
-      </footer>
-    </div>
+        <footer className="w-full text-center text-sm text-gray-400">
+          © 2025 MyLogue
+        </footer>
+      </div>
+    </>
   );
 }
 
